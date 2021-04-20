f9103de421a78e015386efda2af3727c0fe5e8e2
Aditya Singh <aditya.singh@tesco.com>
2021-04-20 10:11:08 +0530

    refactored

--- src/test/resources/queries.sql
+++ src/test/resources/queries.sql
@@ -1,3 +1,4 @@
 SELECT META(`products`).id AS __id, META(`products`).cas AS __cas, `products`.* FROM `products` LIMIT $1 OFFSET $2
 SELECT META(`products`).id AS __id, META(`products`).cas AS __cas, `products`.* FROM `products` LIMIT $1 OFFSET $2
 SELECT META(`products`).id AS __id, META(`products`).cas AS __cas, `products`.* FROM `products` where name = $1 LIMIT $2 OFFSET $3
+SELECT META(`products`).id AS __id, META(`products`).cas AS __cas, `products`.* FROM `products` where size = $1 LIMIT $2 OFFSET $3
